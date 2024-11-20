8db8cf4b624601c07d2dd48589fee77ed3198bcb
Tom <tom@tomsarver.dev>
2024-11-20 14:42:14 -0600

    making home page more mobile friendly

--- components/Header.jsx
+++ components/Header.jsx
@@ -6,7 +6,7 @@ import Socials from "../components/Socials";
 const Header = () => {
   return (
     <header className="absolute z-30 w-full items-center px-16 xl-px-0 xl:h-[90px]">
-      <div className="container mx-auto">
+      <div className="hidden container mx-auto xl:block">
         <div className="flex flex-col lg:flex-row justify-between items-center gap-y-6 py-8">
           {/* logo */}
           <Link href="/">
